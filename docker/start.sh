6d8c559eac9f28bcb1311fad2209181f595766e1
andirgaarya <andirgaarya@gmail.com>
2026-05-08 12:21:46 +0700

    fix: add storage link and permissions

--- docker/start.sh
+++ docker/start.sh
@@ -9,6 +9,11 @@ php artisan storage:link
 # Run migrations
 php artisan migrate --force
 
+# Storage link & permissions
+php artisan storage:link
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+
 # Start PHP-FPM
 php-fpm -D
 
